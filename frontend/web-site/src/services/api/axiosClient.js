c461f734faaf7423e03997265dbca1e9151413de
nguyencongman <0306231404@caothang.edu.vn>
2026-07-01 20:15:55 +0700

    fix

--- frontend/web-site/src/services/api/axiosClient.js
+++ frontend/web-site/src/services/api/axiosClient.js
@@ -8,8 +8,8 @@ import {
 } from "../token/refreshTokenManager";
 
 const axiosClient = axios.create({
- baseURL: "https://smart-green-market-api.onrender.com/api",
-  //baseURL: "http://127.0.0.1:8000/api",
+  //baseURL: "https://smart-green-market-api.onrender.com/api",
+  baseURL: "http://127.0.0.1:8000/api",
   headers: {
     "Content-Type": "application/json",
   },
