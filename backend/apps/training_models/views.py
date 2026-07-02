cd961e0b68e705851dfc347eabefd807a39963f2
ScullyNhat <pnhat563@gmail.com>
2026-07-02 14:12:17 +0700

    module goi y san pham lien quan

--- backend/apps/training_models/views.py
+++ backend/apps/training_models/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from apps.training_models.services import customer_segmentation
+from apps.training_models.services import related_product_recommendation
 
 # Create your views here.
 def index(request):
@@ -9,3 +10,11 @@ def index(request):
     print(f"Clustered DataFrame:\n{df_labeled.head(20)}")
     print(f"mapping: {label_mapping}")
     return HttpResponse(f"Customer Metrics:")
+
+def train_related_products(request):
+    service = related_product_recommendation.RelatedProductRecommendationService()
+    success, message = service.execute_pipeline()
+    if success:
+        return HttpResponse(f"Success: {message}")
+    else:
+        return HttpResponse(f"Error: {message}")
